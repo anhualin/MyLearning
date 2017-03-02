0de27886d6ee1724192339df89cf3c46815da58a
Anhua Lin <anhua.lin@gmail.com>
2017-03-02 17:05:01 -0500

    working on AI proj 3 problem3

--- AI/Project3/problem3.py
+++ AI/Project3/problem3.py
@@ -6,37 +6,36 @@ Created on Mon Jan 30 10:56:17 2017
 """
 
 import numpy as np
-from sklearn import preprocessing
+##from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-import sys
+from sklearn.model_selection import cross_val_score
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+#import sys
 
 def prepare(data):
     X = data[:, 0:2]
     y = data[:,2]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test-size = 0.4, stratify = y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, stratify = y)
+    parameters = {'kernel': ['linear'], 'C':[0.1, 10]}
+    svr = svm.SVC()
+    clf = GridSearchCV(svr, parameters, cv = 10)
+    clf.fit(X_train, y_train)
+    clf.cv_results_
+    clf.score(X_train, y_train)
     
-    X_scaled = preprocessing.scale(X)
-    one = np.ones([X.shape[0], 1])
-    X_new = np.concatenate((one, X_scaled), axis = 1)
-    return X_new, y
-
-def gradientDescent(X, y, f):
-    N, m = X.shape
-    y = y.reshape([N,1])
-    alphas = [.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 5.0, 10.0, 0.02]
-    for alpha in alphas:
-        beta = np.zeros([m,1])
-        for i in range(100):
-            beta = beta -alpha/N * X.T.dot(X.dot(beta) - y)
-        f.write(str(alpha) + ',100,' + str(beta[0,0]) + ',' + str(beta[1,0]) + ',' + str(beta[2,0]) + '\n')
-
+    clf = svm.SVC(kernel='linear', C=1)
+    scores = cross_val_score(clf, X_train, y_train, cv=5)
+    C = [0.1, 0.5, 1, 5, 10, 50, 100]
+    print "C=", C
+    print(" Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 def main(argv):
-    input_file = 'input3.csv' 
+    input_file = 'C:/Users/alin/Documents/SelfStudy/MyLearning/AI/Project3/input3.csv'
+#    input_file = 'input3.csv'
     data = np.loadtxt(input_file, delimiter = ',')
     prepare(data)
-    X = data[:, 0:2]
-    y = data[:, 2]
-    
-  
+
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
