4d9df3d9af970e3f75adf2a190fbc7c7b8092963
Anhua Lin <anhua.lin@gmail.com>
2017-03-17 23:41:01 -0400

    finish ai proj4

--- AI/Project4/driver.py
+++ AI/Project4/driver.py
@@ -6,11 +6,9 @@ Created on Wed Mar 15 16:59:07 2017
 @author: alin
 """
 
+import sys
+from copy import deepcopy
 
-from copy import copy
-state0 = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
-state0 = '000260701680070090190004500820100040004602900050003028009300074040050036703018000'
-state0 = sudoku_starts[2]
 def str2board(state):
     sudoku = {}
     for i in range(9):
@@ -31,8 +29,6 @@ def display(board):
         for j in range(9):
             s += str(board[i, j]) + ' '
         print s
-sudoku = str2board(state0)
-display(sudoku)
 
 def getNeighbor():
     neighbor = {}
@@ -71,7 +67,6 @@ def setup(sudoku):
                 allowed[n].remove(sudoku[(i,j)])
     return unassigned, assigned, allowed, neighbor
 
-unassigned, assigned, allowed, neighbor = setup(sudoku)
 
 def choose_var(allowed):
     """ choose the unassigned with the fewest allowed values """
@@ -89,52 +84,78 @@ def order_domain(allowed, v, neighbor, sudoku):
     return [x for (x,y) in h]                 
 
     
-def backtrack(assigned, allowed, sudoku, neighbor, N):
-    print "N= ", N
+def backtrack(assigned, allowed, sudoku, neighbor):
+   
     if len(assigned) == 81:
         return sudoku
     v = choose_var(allowed)
+  
+    
     domain = order_domain(allowed, v, neighbor, sudoku)
+   
+   
+    assigned.append(v)
     for val in domain:
-        assigned.append(v)
-        allowed1 = copy(allowed)
+      
+        allowed1 = deepcopy(allowed)
         sudoku[v] = val
         del allowed1[v]
         for n in neighbor[v]:
-            if sudoku[n] == 0 and val in allowed[n]:
-                allowed[n].remove(val)
+            if sudoku[n] == 0 and val in allowed1[n]:
+                allowed1[n].remove(val)
         result = backtrack(assigned, allowed1, sudoku, neighbor)
         if result:
             return result
-        else:
-            assigned.remove(v)
-            sudoku[v] = 0
+    assigned.remove(v)
+    sudoku[v] = 0
+  
     return False
 
-sboard = backtrack(assigned, allowed, sudoku, neighbor)
 
-def load_data():
-    start_file = 'C:/Users/alin/Documents/SelfStudy/MyLearning/AI/Project4/sudokus_start.txt'
-    with open(start_file) as f:
-        sudoku_starts = f.readlines()
-    sudoku_starts = [x.strip() for x in sudoku_starts]
-                     
-    finish_file = 'C:/Users/alin/Documents/SelfStudy/MyLearning/AI/Project4/sudokus_finish.txt'
-    with open(finish_file) as f:
-        sudoku_finishes = f.readlines()
-    sudoku_finishes = [x.strip() for x in sudoku_finishes]
-    return sudoku_starts, sudoku_finishes
-sudoku_starts, sudoku_finishes = load_data()                 
 
-total_error = 0
-for i in range(2):
-    sudoku = str2board(sudoku_starts[i])
+#def load_data():
+#    #start_file = 'C:/Users/alin/Documents/SelfStudy/MyLearning/AI/Project4/sudokus_start.txt'
+#    start_file = 'sudokus_start.txt'
+#    with open(start_file) as f:
+#        sudoku_starts = f.readlines()
+#    sudoku_starts = [x.strip() for x in sudoku_starts]
+#                     
+#    #finish_file = 'C:/Users/alin/Documents/SelfStudy/MyLearning/AI/Project4/sudokus_finish.txt'
+#    finish_file = 'sudokus_finish.txt'
+#    with open(finish_file) as f:
+#        sudoku_finishes = f.readlines()
+#    sudoku_finishes = [x.strip() for x in sudoku_finishes]
+#    return sudoku_starts, sudoku_finishes
+#sudoku_starts, sudoku_finishes = load_data()                 
+#
+#state0 = sudoku_starts[3]
+#sudoku = str2board(state0)
+#display(sudoku)
+#unassigned, assigned, allowed, neighbor = setup(sudoku)
+#sboard = backtrack(assigned, allowed, sudoku, neighbor)
+
+def main(argv):
+    state = argv[0]
+    sudoku = str2board(state)
     unassigned, assigned, allowed, neighbor = setup(sudoku)
     solution = backtrack(assigned, allowed, sudoku, neighbor)
-    if solution:
-        sol_str = board2str(solution)
-        if sol_str != sudoku_finishes[i]:
-            total_error += 1
-    else:
-        total_error += 1
-print "total_error = ", total_error
+    sol_str = board2str(solution)
+    f = open('output.txt', 'w')
+    f.write(sol_str)
+    f.close()
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    
+    
+#total_error = 0
+#for i in range(len(sudoku_starts)):
+#    sudoku = str2board(sudoku_starts[i])
+#    unassigned, assigned, allowed, neighbor = setup(sudoku)
+#    solution = backtrack(assigned, allowed, sudoku, neighbor)
+#    if solution:
+#        sol_str = board2str(solution)
+#        if sol_str != sudoku_finishes[i]:
+#            total_error += 1
+#    else:
+#        total_error += 1
+#print "total_error = ", total_error
