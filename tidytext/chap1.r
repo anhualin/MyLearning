ede0711f476fbcd0b1f7433bef43b2e58d58c1d6
Anhua Lin <anhua.lin@laureate.net>
2017-03-30 15:21:54 -0400

    tidytext

--- tidytext/chap1.r
+++ tidytext/chap1.r
@@ -81,3 +81,35 @@ library(tidytext)
 
 text_df %>% 
   unnest_tokens(word, text)
+
+library(janeaustenr)
+#library(dplyr)
+library(stringr)
+
+original_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
+                                                 ignore_case = TRUE)))) %>%
+  ungroup()
+
+original_books
+
+tidy_books <- original_books %>%
+  unnest_tokens(word, text)
+
+data("stop_words")
+tidy_books <- tidy_books %>%
+  anti_join(stop_words)
+
+tidy_books %>%
+  count(word, sort = TRUE)
+
+tidy_books %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + 
+  geom_col() + 
+  xlab(NULL) +
+  coord_flip()
