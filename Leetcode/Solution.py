91ef90c7d38ec7f36b85f124a555c5b2f1a7f007
Anhua Lin <anhua.lin@gmail.com>
2017-04-02 22:36:09 -0400

    leetcode

---
+++ Leetcode/Solution.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  2 15:00:52 2017
+
+@author: alin
+"""
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        len1 = len(nums1)
+        len2 = len(nums2)
+        if (len1 + len2) % 2 == 0:
+            l1 = 0
+            u1 = len1 - 1
+            l2 = 0
+            u2 = len2 - 1
+            x1 = 0
+            y1 = 0
+            
