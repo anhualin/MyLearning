a2e045b448cf7ee63b8176d294cc3d817fb8f72f
Anhua Lin <anhua.lin@laureate.net>
2017-04-03 15:49:05 -0400

    leetcode

--- Leetcode/Solution.py
+++ Leetcode/Solution.py
@@ -16,10 +16,37 @@ class Solution(object):
         len1 = len(nums1)
         len2 = len(nums2)
         if (len1 + len2) % 2 == 0:
+            half = (len1 + len2 - 2) / 2
             l1 = 0
             u1 = len1 - 1
             l2 = 0
             u2 = len2 - 1
-            x1 = 0
-            y1 = 0
-            
+           
+            x = int((l1 + u1)/2)
+            y = int((l2 + u2)/2)
+            print 'x=', x
+            print 'y=', y
+            # easy case
+            # general cases
+            while(u1 - l1 >= 2 and u2 - l2 >= 2):
+                print nums1[x], nums2[y], x, y, half
+                if nums1[x] >= nums2[y]:
+                    if x + y >= half:
+                        u1 = x
+                        x = int((l1 + u1)/2)
+                    else:
+                        l2 = y
+                        y = int((l2 + u2)/2)                           
+                else:
+                    if x + y >= half:
+                        u2 = y
+                        y = int((l2 + u2)/2)
+                    else:
+                        l1 = x
+                        x = int((l1 + u1)/2)
+            print l1, u1, l2, u2
+            print nums1[l1], nums1[u1]
+            print nums2[l2], nums2[u2]
+
+a = Solution()
+a.findMedianSortedArrays([1,1,1,1], [2,2,2,2,2,2])    
