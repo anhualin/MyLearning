11cfaee2092f78eab45b5591f24c3913d49ca844
Anhua Lin <anhua.lin@laureate.net>
2017-04-10 15:15:51 -0400

    Solution java

--- Leetcode/Solution.java
+++ Leetcode/Solution.java
@@ -2,39 +2,25 @@ public class Solution {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-	String a = "au";
+	int[] nums1 = { 
+		    100, 200, 300,
+		    400, 500, 600, 
+		    700, 800, 900, 1000
+	};
+	int[] nums2 = { 
+		    100, 200, 300,
+		    400, 500, 600, 
+		    700, 800, 900, 1000
+	};
 	
-	System.out.println(lengthOfLongestSubstring(a));
+	System.out.println(findMedianSortedArrays(nums1, nums2));
     }
     
-    public static int lengthOfLongestSubstring(String s) {
-	if (s.length() == 0){
-	    return 0;
-	}
-	
-	int bestLength = 1;
-	int currentStart = 0;
-	int currentEnd = 1;
-	int currentLength = 1;
-	while(currentEnd < s.length()){
-	   
-	    char c = s.charAt(currentEnd);
-	    
-	    int pos = s.substring(currentStart, currentEnd).indexOf(c);
-	   
-	    if (pos >= 0){
-		currentLength = currentEnd - currentStart;
-		bestLength = currentLength > bestLength ? currentLength: bestLength;
-		currentStart = currentStart + pos + 1;
-	    }
-	    currentEnd += 1;
-	}
-	
-	currentLength = currentEnd - currentStart;
-	bestLength = currentLength > bestLength ? currentLength: bestLength;
-	return bestLength;
-	    
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    	return 1.0;
     }
+    
+   
 }
 
 
