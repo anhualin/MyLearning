9f5d5c9fb8139c51b3f237bd2216e67ab47876fe
Anhua Lin <anhua.lin@gmail.com>
2017-04-11 23:09:58 -0400

    leet code

--- Leetcode/Solution.java
+++ Leetcode/Solution.java
@@ -1,22 +1,39 @@
+import java.util.*;
 public class Solution {
-
+    
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-	int[] nums1 = { 
-		    100, 200, 300,
-		    400, 500, 600, 
-		    700, 800, 900, 1000
-	};
-	int[] nums2 = { 
-		    1, 200, 300,
-		    400, 500, 600, 
-		    700, 800, 900
-	};
-	
-	System.out.println(findMedianSortedArrays(nums1, nums2));
+       
+	Random ran = new Random();
+	int N1 = ran.nextInt(100) + 1;
+	int N2 = ran.nextInt(100) + 1;
+	int[] a3 = new int[N1 + N2];
+	int[] a1 = new int[N1];
+	for(int i = 0; i < N1; i++){
+	    a1[i] = ran.nextInt(100);
+	    a3[i] = a1[i];
+	}
+	Arrays.sort(a1);
+	int[] a2 = new int[N2];
+	for(int i = 0; i < N2; i++){
+	    a2[i] = ran.nextInt(100);
+	    a3[N1 + i] = a2[i];
+	}
+	Arrays.sort(a2);
+	Arrays.sort(a3);
+	double result = findMedianSortedArrays(a1, a2);
+	double r;
+	if ((N1 + N2)%2 == 0){
+	    r = (double)(a3[(N1 + N2)/2 - 1] + a3[(N1 + N2)/2])/2.0;
+	}else{
+	    r = (double)(a3[(N1 + N2)/2]);
+	}
+	System.out.println(N1);
+	System.out.println(N2);
+	System.out.println(result -r);
     }
     
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+	
 	int len1 = nums1.length;
 	int len2 = nums2.length;
 	
@@ -121,14 +138,112 @@ public class Solution {
 	
 		    
 	}
-	System.out.println(nums1[l1]);
-	System.out.println(nums1[u1]);
-	System.out.println(nums2[l2]);
-	System.out.println(nums2[u2]);
+
+	int gotten = 0;
+	int total = 0;
+	int [] candidates = {nums1[l1], nums1[u1], nums2[l2], nums2[u2]};
+	if ((len1 + len2) % 2 == 0){
+	    
+	    for(int e: candidates){
+		Positions pos = findPos(e, nums1, nums2);
+		int pos0 = pos.getLeft();
+		int pos1 = pos.getRight();
+		if (pos0 <= half && pos1 >= half + 1){
+		    return (double)e;
+		}
+		if ((pos0 <= half && pos1 >= half) ||
+		    (pos0 <= half + 1 && pos1 >= half + 1)){
+		    if (gotten == 0 || total != e){
+			gotten ++;
+			total += e;
+			if (gotten == 2){
+			return (double)(total)/2.0;
+			}
+		    }
+		}
+	    }
+	}else{
+	    for(int e: candidates){
+		Positions pos = findPos(e, nums1, nums2);
+		int pos0 = pos.getLeft();
+		int pos1 = pos.getRight();
+		if (pos0 <= half + 1 && pos1 >= half + 1){
+		    return (double) e;
+		}
+	    }
+	}
+	
+	
 	return (double)(len1 + len2);
     }
     
-   
+    public static Positions findPos(int x, int[] nums1, int[] nums2){
+	int pl1 = findLower(x, nums1);
+	int pl2 = findLower(x, nums2);
+	int pu1 = findUpper(x, nums1);
+	int pu2 = findUpper(x, nums2);
+	int left = pl1 + pl2 + 3;
+	int right = pu1 + pu2;
+	return new Positions(left, right);
+	
+    }
+    public static int findLower(int x, int[] nums){
+	int N = nums.length;
+	int pl, pu;
+	if (x <= nums[0]){
+	    return -1;
+	}else if (x > nums[N - 1]){
+	    return N - 1;
+	}else{
+	    pl = 0;
+	    pu = N - 1;
+	    while(pu - pl > 1){
+		int z = (pu + pl)/2;
+		if (x > nums[z]){
+		    pl = z;
+		}else{
+		    pu = z;
+		}
+	    }
+	}
+	return pl;
+    }
+    public static int findUpper(int x, int[] nums){
+	int N = nums.length;
+	int pl, pu;
+	if (x < nums[0]){
+	    return 0;
+	}else if (x >= nums[N - 1]){
+	    return N;
+	}else{
+	    pl = 0;
+	    pu = N - 1;
+	    while(pu - pl > 1){
+		int z = (pu + pl)/2;
+		if (x < nums[z]){
+		    pu = z;
+		}else{
+		    pl = z;
+		}
+	    }
+	}
+	return pu;
+    }
 }
 
 
+  class Positions{
+     private int left;
+     private int right;
+     Positions(int left, int right){
+	 this.left = left;
+	 this.right = right;
+     }
+     int getLeft(){
+	 return left;
+     }
+     int getRight(){
+	 return right;
+     }
+  }
+	
