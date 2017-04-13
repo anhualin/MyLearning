6e21c69810197fcf0c78d70a89acb0683ea0aca3
Anhua Lin <anhua.lin@gmail.com>
2017-04-12 23:40:43 -0400

    tidy

---
+++ tidytext/chap4.r
@@ -0,0 +1,27 @@
+library(dplyr)
+library(tidytext)
+library(janeaustenr)
+library(tidyr)
+
+austen_bigrams <- austen_books() %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2)
+
+austen_bigrams
+
+austen_bigrams %>% 
+  count(bigram, sort = TRUE)
+
+bigrams_separated <- austen_bigrams %>%
+  separate(bigram, c("word1", "word2"), sep = " ")
+
+bigrams_filtered <- bigrams_separated %>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word)
+
+bigram_counts <- bigrams_filtered %>% 
+  count(word1, word2, sort = TRUE)
+
+bigram_counts
+
+bigrams_united <- bigrams_filtered %>%
+  unite(bigram, word1, word2, sep = " ")
