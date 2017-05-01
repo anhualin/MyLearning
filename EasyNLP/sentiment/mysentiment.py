a7fdbfdfc3abbaf42e9c0c9c6e52b0bf9e71eba8
Anhua Lin <anhua.lin@gmail.com>
2017-04-30 21:35:34 -0400

    sentiment

---
+++ EasyNLP/sentiment/mysentiment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 30 21:29:10 2017
+
+@author: alin
+"""
+
+from BeautifulSoup import *
+positive_reviews = BeautifulSoup(open('positive.review.txt').read())
