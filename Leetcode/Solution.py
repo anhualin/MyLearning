50aba7cd2a8ab8f9cf6fdb28e72b3df76383aafa
Anhua Lin <anhua.lin@laureate.net>
2017-05-02 15:16:27 -0400

    leet code

--- Leetcode/Solution.py
+++ Leetcode/Solution.py
@@ -7,33 +7,17 @@ Created on Sun Apr  2 15:00:52 2017
 """
 
 class Solution(object):
-   def convert(self, s, numRows):
+  def reverse(self, x):
         """
-        :type s: str
-        :type numRows: int
-        :rtype: str
+        :type x: int
+        :rtype: int
         """
-        if numRows <= 1:
-            return s
-        zig = ['' for i in range(len(s))]
-        current = 0
-        for i in range(numRows):
-            if i == 0:
-                step = [2*numRows - 2, 2*numRows - 2]
-            elif i == numRows - 1:
-                step = [2*i, 2*i]
-            else:
-                step = [2*(numRows - 1 - i), 2*i]
-            
-            assign = i
-            j = 0
-            while assign < len(s):
-                zig[current] = s[assign]
-                assign += step[j]
-                current += 1
-                j = (j+1) % 2
-         
-        return ''.join(zig)
-                    
+        sign = -1 if x < 0  else 1
+        a = str(x)
+        y = a[1:len(a)] if x < 0  else a
+        z = sign * int(y[len(y)-1:0:-1] + y[0])
+        if z < -2147483648 or z > 2147483647:
+            return 0
+        return  z
 sol = Solution()
-print sol.convert("PAYPALISHIRING", 2)
+print sol.reverse(1534236469)
