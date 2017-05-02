8d3471034f5fb2443655614b82046f2064ca459d
Anhua Lin <anhua.lin@gmail.com>
2017-05-01 23:35:31 -0400

    leetcode

--- Leetcode/Solution.py
+++ Leetcode/Solution.py
@@ -7,45 +7,33 @@ Created on Sun Apr  2 15:00:52 2017
 """
 
 class Solution(object):
-     def longestPalindrome(self, s):
+   def convert(self, s, numRows):
         """
         :type s: str
+        :type numRows: int
         :rtype: str
         """
-        LS = len(s)
-        if LS <= 1:
+        if numRows <= 1:
             return s
-        curr = 0
-        bestCenter = 0
-        if s[0] == s[1]:
-            bestLength = 2
-        else:
-            bestLength = 1
-        curr += 1
-        while curr + bestLength/2 < LS:
-            #check odd palindrome centered at curr
-            half = bestLength / 2 if bestLength % 2 == 0 else bestLength / 2 + 1
-            if curr - half >= 0 and curr + half < LS and s[curr - half: curr] == s[curr + half: curr: -1]:
-                half += 1
-                while curr - half >=0 and curr + half < LS and s[curr - half] == s[curr + half]:
-                    half += 1
-                half -= 1
-                bestLength = 2 * half + 1
-                bestCenter = curr
-            #check even palindrome centered at curr
-            half = bestLength / 2 + 1
-            if curr - half + 1 >= 0 and curr + half < LS and s[curr - half + 1: curr + 1] == s[curr + half: curr: -1]:
-                half += 1
-                while curr - half + 1 >= 0 and curr + half < LS and s[curr - half + 1] == s[curr + half]:
-                    half +=1
-                half -=1
-                bestLength = 2 * half
-                bestCenter = curr
-            curr += 1
-        half = bestLength / 2
-        palindrome = s[bestCenter - half + 1: bestCenter + half + 1] if bestLength % 2 == 0 else s[bestCenter - half: bestCenter + half + 1]    
-        return palindrome
-                    
+        zig = ['' for i in range(len(s))]
+        current = 0
+        for i in range(numRows):
+            if i == 0:
+                step = [2*numRows - 2, 2*numRows - 2]
+            elif i == numRows - 1:
+                step = [2*i, 2*i]
+            else:
+                step = [2*(numRows - 1 - i), 2*i]
+            
+            assign = i
+            j = 0
+            while assign < len(s):
+                zig[current] = s[assign]
+                assign += step[j]
+                current += 1
+                j = (j+1) % 2
+         
+        return ''.join(zig)
                     
 sol = Solution()
-print sol.longestPalindrome('cbbdxcdefggfedc12345689098654321c2')
+print sol.convert("PAYPALISHIRING", 2)
