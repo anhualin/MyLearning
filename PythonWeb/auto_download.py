142619084d3529595d85509927e9b09700173b85
Anhua Lin <anhua.lin@gmail.com>
2017-05-03 22:50:07 -0400

    auto download

--- PythonWeb/auto_download.py
+++ PythonWeb/auto_download.py
@@ -5,22 +5,32 @@ Created on Wed May 03 10:29:05 2017
 @author: alin
 """
 
-
+# Simple file to download some spanish mp3 from sbs website
 
 import urllib
+import urllib2
 import re
 from bs4 import BeautifulSoup 
+import os
 
 url = 'http://www.sbs.com.au/podcasts/yourlanguage/spanish/'
 html = urllib.urlopen(url).read()
-
 soup = BeautifulSoup(html)
 
-# Retrieve all of the anchor tags
-tags = soup('span')
-total = 0
-for tag in tags:
-    # Look at the parts of a tag
-    num = re.findall('(\d+)', str(tag))
-    total += int(num[0])
-print total
+store = []
+for link in soup.find_all('a'):
+    address = link.get('href')
+    if re.findall('\.mp3', address):
+        store.append(address)
+        
+folder = "/home/alin/SpanishMp3/"
+file_in_folder = os.listdir(folder)
+for link in store:
+    filename = re.findall('spanish_\d+_\d+\.mp3', link)[0]
+    print filename 
+    if filename not in file_in_folder:
+        mp3file = urllib2.urlopen(link)
+        to_file = folder + filename
+        with open(to_file, "wb") as output:
+            output.write(mp3file.read())
+        
