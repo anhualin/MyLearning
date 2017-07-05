8777f04d78cfac74fa5011bec0d2326397c55014
Anhua Lin <anhua.lin@gmail.com>
2017-07-04 23:18:22 -0400

    cs231n

--- CS231n/toy.py
+++ CS231n/toy.py
@@ -23,3 +23,42 @@ for j in range(K):
     
 plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
 plt.show()
+
+
+# linear classifier
+W = 0.01 * np.random.randn(D, K)
+b = np.zeros((1,K))
+
+step_size = 1.0
+reg = 1e-3
+
+num_examples = X.shape[0]
+for i in range(200):
+    scores = np.dot(X, W) + b
+    
+    exp_scores = np.exp(scores)
+    probs = exp_scores / np.sum(exp_scores, axis = 1, keepdims = True)
+    
+    corect_logprobs = -np.log(probs[range(num_examples), y])
+    data_loss = np.sum(corect_logprobs) / num_examples
+    reg_loss = 0.5 * reg * np.sum(W*W)
+    loss = data_loss + reg_loss
+    if i % 10 == 0:
+        print("iteration %d: loss %f", (i, loss))
+    
+    dscores = probs
+    dscores[range(num_examples), y] -= 1
+    dscores /= num_examples
+
+    dW = np.dot(X.T, dscores)
+    db = np.sum(dscores, axis = 0, keepdims = True)
+    
+    dW += reg*W
+    W += -step_size * dW
+    b += -step_size * db
+    
+    
+    
+scores = np.dot(X, W) + b
+predicted_class = np.argmax(scores, axis = 1)
+print("training accuracty: %.2f" % (np.mean(predicted_class == y)))        
