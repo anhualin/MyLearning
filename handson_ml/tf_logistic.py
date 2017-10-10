e914b0b2c8a84b037af4e1997dbdcdc46d81adc5
Anhua Lin <anhua.lin@gmail.com>
2017-10-09 20:31:08 -0400

    tf logistic

---
+++ handson_ml/tf_logistic.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 17:00:36 2017
+
+@author: alin
+"""
+
+import numpy as np
+import tensorflow as tf
+
+X = tf.placeholder(None, dtype=tf.float32)
+y = tf.placeholder(None, dtype=tf.int32)
+
