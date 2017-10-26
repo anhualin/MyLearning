c52c8c2f343fc5e11f518e750d195aeff240ce05
Anhua Lin <anhua.lin@gmail.com>
2017-10-25 23:27:16 -0400

    tf

--- handson_ml/chap11_8.py
+++ handson_ml/chap11_8.py
@@ -10,6 +10,8 @@ from sys import platform
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
+from functools import partial
+
 
 if platform == 'win32':
     tmpdir = 'C:/Users/alin/Documents/Temp/data'
@@ -44,30 +46,65 @@ n_hidden4 = 100
 n_hidden5 = 100
 n_outputs = 5
 
+best_accuracy = {}
+
+learning_rate = 0.005
+batch_norm_momentum = 0.9
+dropout_rate = 0.5
+losses = []
 reset_graph()
 
 X = tf.placeholder(tf.float32, shape=(None, n_inputs), name='X')
 y = tf.placeholder(tf.int64, shape=(None), name='y')
+training = tf.placeholder_with_default(False, shape=(), name='training')
 
 with tf.name_scope('dnn'):
+    
     he_init = tf.contrib.layers.variance_scaling_initializer()
-    hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.elu, 
-                              kernel_initializer=he_init, name='hidden1')
-    hidden2 = tf.layers.dense(hidden1, n_hidden2, activation=tf.nn.elu, 
-                              kernel_initializer=he_init, name='hidden2')
-    hidden3 = tf.layers.dense(hidden2, n_hidden3, activation=tf.nn.elu, 
-                              kernel_initializer=he_init, name='hidden3')
-    hidden4 = tf.layers.dense(hidden3, n_hidden4, activation=tf.nn.elu, 
-                              kernel_initializer=he_init, name='hidden4')
-    hidden5 = tf.layers.dense(hidden4, n_hidden5, activation=tf.nn.elu, 
-                              kernel_initializer=he_init, name='hidden5')
-    logits = tf.layers.dense(hidden5, n_outputs, kernel_initializer=he_init, name='output')
+    
+    my_batch_norm_layer = partial(
+            tf.layers.batch_normalization,
+            training=training,
+            momentum = batch_norm_momentum
+            )
+    my_dense_layer = partial(
+            tf.layers.dense,
+            kernel_initializer=he_init)
+    hidden1 = my_dense_layer(X, n_hidden1, name='hidden1')
+   # hidden1_drop = tf.layers.dropout(hidden1, dropout_rate, training=training)
+    bn1 = tf.nn.elu(my_batch_norm_layer(hidden1), name='bn1')
+    hidden2 = my_dense_layer(bn1, n_hidden2, name='hidden2')
+#    hidden2_drop = tf.layers.dropout(hidden2, dropout_rate, training=training)
+    bn2 = tf.nn.elu(my_batch_norm_layer(hidden2), name='bn2')
+    hidden3 = my_dense_layer(bn2, n_hidden3, name='hidden3')
+#    hidden3_drop = tf.layers.dropout(hidden3, dropout_rate, training=training)
+    bn3 = tf.nn.elu(my_batch_norm_layer(hidden3), name='bn3')
+    hidden4 = my_dense_layer(bn3, n_hidden4, name='hidden4')
+#    hidden4_drop = tf.layers.dropout(hidden4, dropout_rate, training=training)
+    bn4 = tf.nn.elu(my_batch_norm_layer(hidden4), name='bn4')
+    hidden5 = my_dense_layer(bn4, n_hidden5, name='hidden5')
+#    hidden5_drop = tf.layers.dropout(hidden5, dropout_rate, training=training)
+    bn5 = tf.nn.elu(my_batch_norm_layer(hidden5), name='bn5')
+    logits_before_bn = my_dense_layer(bn5, n_outputs, name='output')
+    logits = my_batch_norm_layer(logits_before_bn)
+#    
+#    hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.elu, 
+#                              kernel_initializer=he_init, name='hidden1')
+#    hidden2 = tf.layers.dense(hidden1, n_hidden2, activation=tf.nn.elu, 
+#                              kernel_initializer=he_init, name='hidden2')
+#    hidden3 = tf.layers.dense(hidden2, n_hidden3, activation=tf.nn.elu, 
+#                              kernel_initializer=he_init, name='hidden3')
+#    hidden4 = tf.layers.dense(hidden3, n_hidden4, activation=tf.nn.elu, 
+#                              kernel_initializer=he_init, name='hidden4')
+#    hidden5 = tf.layers.dense(hidden4, n_hidden5, activation=tf.nn.elu, 
+#                              kernel_initializer=he_init, name='hidden5')
+#    logits = tf.layers.dense(hidden5, n_outputs, kernel_initializer=he_init, name='output')
 
 with tf.name_scope('loss'):
     xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
     loss = tf.reduce_mean(xentropy, name="loss")
         
-learning_rate = 0.001
+
 with tf.name_scope('train'):
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     training_op = optimizer.minimize(loss)
@@ -81,26 +118,32 @@ saver = tf.train.Saver()
 n_epochs = 20
 batch_size = 200
 n_batchs = X_train.shape[0] // batch_size
-best_accuracy = -np.inf
+best_accuracy[learning_rate] = (-np.inf, -np.inf)
 no_improve = 0
 threshold = 4
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 with tf.Session() as sess:
     sess.run(init)
     for epoch in range(n_epochs):
+        
         if no_improve < threshold:
             for batch_index in range(n_batchs):
                 X_batch, y_batch = mnist.train.next_batch(batch_size)
                 X_batch1 = X_batch[y_batch < 5]
                 y_batch1 = y_batch[y_batch < 5]
-                sess.run(training_op, feed_dict={X: X_batch1, y: y_batch1})
+                sess.run([training_op, extra_update_ops], feed_dict={training: True, X: X_batch1, y: y_batch1})
+            losses.append(loss.eval({X: X_train1, y:y_train1}))
             accuracy_val = accuracy.eval({X: X_valid1, y:y_valid1})
-            print(epoch, '  Valid accuracy:',  accuracy_val)
-            if accuracy_val > best_accuracy:
-                best_accuracy = accuracy_val
+            accuracy_train = accuracy.eval({X: X_train1, y:y_train1})
+            print(epoch, 'Train accuracy:', accuracy_train,  '  Valid accuracy:',  accuracy_val)
+            if accuracy_val > best_accuracy[learning_rate][1]:
+                best_accuracy[learning_rate] = (accuracy_train, accuracy_val)
                 save_path = saver.save(sess, "/tmp/chap11_8.ckpt")
                 no_improve = 0
             else:
                 no_improve += 1
-            
+with tf.Session() as sess:
+    saver.restore(sess, "/tmp/chap11_8.ckpt")
+    accuracy_test = accuracy.eval({X:X_test1, y:y_test1})
+    print('test accuracy:', accuracy_test)
     
-    #0.9933542  lrate = 0.001
